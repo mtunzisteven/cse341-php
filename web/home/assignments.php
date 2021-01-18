f2e7530ba58b928c2ec1056f0f5195d15f82d70b
mtunzisteven <mtunzism@byui.edu>
2021-01-18 19:23:57 +0200

    style sheet

--- web/home/assignments.php
+++ web/home/assignments.php
@@ -5,7 +5,9 @@
   <meta charset="utf-8">
   <meta content="author" name="Mtunzi Mavuma" >
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="style.css">
+
+  <?php echo '<link rel="stylesheet" href="style.css" type="text/css">'; ?>
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
  </head>
  <body>
