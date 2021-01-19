60dabc09ffb2a60ba5a61a22a7efed696e442bb6
mtunzisteven <mtunzism@byui.edu>
2021-01-19 10:54:52 +0200

    Shortcode

--- web/home/assignments.php
+++ web/home/assignments.php
@@ -1,45 +1,28 @@
-
 <?php
    
-    $nav_template = "nav.php";
-    $footer_template = "footer.php";
+   $nav_template = "nav.php";
+   $footer_template = "footer.php";
 
-    #check whether template exists, if not, cancel program.
-    if(file_exists($nav_template) and file_exists($footer_template)){
-      $nav = file_get_contents($nav_template);
-      $footer = file_get_contents($footer_template);
+   #check whether templates exists, if not, cancel program.
+   if(file_exists($nav_template) and file_exists($footer_template)){
+     $nav = file_get_contents($nav_template);
+     $footer = file_get_contents($footer_template);
     }
     else{
 	    die("File not found!");
     }
 
-    #Set page title
-    $short_code = array(
-      "{PAGE}" => "Assignments"
-    );
-
-    $title = $short_code['{PAGE}'];
-
-    #Replace short codes on teplate with actual desired data
-    foreach(array_keys($short_code) as $k){
-      if(strlen($k)>2 && trim($k)!=""){
-          $content = str_replace($k,$short_code[$k],$content);
-      }
-      else{
-          echo "short codes error!";
-      }
-    }
-
     #Print/Display the navigation template
     echo $nav;
-?>
 
-<div class="container">
-     <h2>Coming soon...</h2>
-</div>
+   ?>
 
-<?php
+  <div class="container">
+      <h2>Coming soon...</h2>
+  </div>
+
+  <?php
 
     echo $footer;
 
-?>
+  ?>
--- web/home/index.php
+++ web/home/index.php
@@ -1,14 +1,21 @@
    <?php
    
-    $nav_template = "nav.php";
+   $nav_template = "nav.php";
+   $footer_template = "footer.php";
 
-    #check whether template exists, if not, cancel program.
-    if(file_exists($nav_template)){
-      $nav = file_get_contents($nav_template);
+   #check whether templates exists, if not, cancel program.
+   if(file_exists($nav_template) and file_exists($footer_template)){
+     $nav = file_get_contents($nav_template);
+     $footer = file_get_contents($footer_template);
     }
     else{
 	    die("File not found!");
     }
+
+    #Replace page title
+    $page_name = "Home";
+
+    $nav = str_replace ( "{PAGE}" , $page_name , $nav );
 
     #Print/Display the navigation template
     echo $nav;
@@ -18,12 +25,8 @@
    <div class="container">
    </div>
 
-   <footer>
-        <div class="footer-div">
-            <a href="https://byui.edu">&copy; 2021 BYU-Idaho | CSE341</a>
-        </div>
-   </footer>
+  <?php
 
- <script src="functions.js"></script>
- </body>
-</html>
+    echo $footer;
+
+  ?>
