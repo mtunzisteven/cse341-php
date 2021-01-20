8ae5d406c89ed26c55d5d57547e2ff74286e4da0
mtunzisteven <mtunzism@byui.edu>
2021-01-20 06:33:22 +0200

    function

--- web/home/functions.js
+++ web/home/functions.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("body").on("load",function(){$(".hero-div").fadeIn(2000);$(".container").slideDown(2000);});});
+$(document).ready(function(){$("body").on("load",function(){$(".hero-div").fadeIn(3500);});});
