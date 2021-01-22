af0731c752f6ef2ad74d4f387821aa24a72a30ba
mtunzisteven <mtunzism@byui.edu>
2021-01-22 16:49:57 +0200

    continents

--- web/form/handler.php
+++ web/form/handler.php
@@ -17,11 +17,11 @@
  <body>
 
     <?php echo '<h4>User Name: '.$_POST["name"].'</h4>'; 
-          echo '<h4>User Email Adress: <a href="mailto:"'.$_POST["email"].'>'.$_POST['email'].'</a></h4>'; 
+          echo '<h4>User Email Address: <a href="mailto:"'.$_POST["email"].'>'.$_POST['email'].'</a></h4>'; 
           echo '<h4>Major: '.$_POST["major"].'</h4>';
           echo '<h4>Comments: '.$_POST["comments"].'</h4>';
 
-          echo '<h4>Continents Visited: '.implode(",",$_POST["continent"]).'</h4>';
+          echo '<h4>Continents Visited: '.implode(",",$_POST["continents"]).'</h4>';
     ?>
 
  </body>
