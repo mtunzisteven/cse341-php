8d67c7807d835e9cfffa4781aa5b4e834f0734b9
mtunzisteven <mtunzism@byui.edu>
2021-01-22 17:04:24 +0200

    handler

--- web/form/handler.php
+++ web/form/handler.php
@@ -21,7 +21,12 @@
           echo '<h4>Major: '.$_POST["major"].'</h4>';
           echo '<h4>Comments: '.$_POST["comments"].'</h4>';
 
-          echo '<h4>Continents Visited: '.implode(",",$_POST["continents"]).'</h4>';
+          echo '<h4>Continents Visited: ';
+          
+          foreach($_POST["continents"] as $visited){
+            echo $visited.'</h4><br/>';
+          }
+              
     ?>
 
  </body>
