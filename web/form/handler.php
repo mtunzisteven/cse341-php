3bf1411c298c80129e68f4fa54c67c1e4f446f0b
mtunzisteven <mtunzism@byui.edu>
2021-01-22 17:29:39 +0200

    paragraph size

--- web/form/handler.php
+++ web/form/handler.php
@@ -6,7 +6,7 @@
   <style>
       body{
           background-color: tomato;
-          padding: 15% 30%;
+          padding: 15% 20%;
           text-align: left;
           color:blanchedalmond;
       }
@@ -16,10 +16,10 @@
  </head>
  <body>
 
-    <?php echo '<h4>User Name: '.$_POST["name"].'</h4>'; 
+    <?php echo '<h4>User Name: <p>'.$_POST["name"].'</p></h4>'; 
           echo '<h4>User Email Address: <a href="mailto:"'.$_POST["email"].'>'.$_POST['email'].'</a></h4>'; 
-          echo '<h4>Major: '.$_POST["major"].'</h4>';
-          echo '<h4>Comments: '.$_POST["comments"].'</h4>';
+          echo '<h4>Major: <p>'.$_POST["major"].'</p></h4>';
+          echo '<h4>Comments: <p>'.$_POST["comments"].'</p></h4>';
         
           if(!empty($_POST["continents"])){
 
@@ -29,7 +29,7 @@
                 #echo '<h5>'.$visited.'</h5>';
             #}
 
-            echo '<h4>Continents Visited: '.implode(", ",$_POST["continents"]).'</h4>';
+            echo '<h4>Continents Visited: <p>'.implode(", ",$_POST["continents"]).'</p></h4>';
           }
               
     ?>
