33a741349a6e2aa4f22b17d13a8748f7a2952679
mtunzisteven <mtunzism@byui.edu>
2021-01-22 18:40:25 +0200

    majors

---
+++ web/form/form.php
@@ -0,0 +1,93 @@
+<!DOCTYPYE html>
+<html lang="en-us">
+ <head>
+  <title>form</title>
+  <meta charset="utf8">
+  <style>
+      body{
+          background-color: tomato;
+          padding: 5px 30%;
+      }
+
+      form{
+          
+          background-color: teal;
+          padding:20%;
+
+      }
+
+      .long{
+          width:100%;
+      }
+
+      button{
+          background-color:yellow;
+          
+      }
+
+  </style>
+ </head>
+ <body>
+
+  <form method="POST" action="handler.php">
+      <label class="long">
+        <div>Name</div>
+        <input class="long" type="text" name="name" />
+      </label><br/>
+      <label class="long">
+        <div>Email</div>
+        <input class="long" type="text" name="email" />
+      </label><br/><hr/>
+        <?php
+
+            $major = ["Computer Science", 
+                      "Web Design and Development", 
+                      "Computer information Technology", 
+                      "Computer Engineering"
+                     ];
+      
+            echo '<div>Majors</div>';
+            foreach($majors as $major){
+            echo '<label><input id="major" type="radio" name="major" value='.$major.'/>'.$major.'</label><br/>';
+            }
+        ?>
+
+        <div>Continents Visited</div>
+        <label>
+            <input type="checkbox" name="continents[]" value="na" />
+            North America
+        </label><br/>
+        <label>
+            <input type="checkbox" name="continents[]" value="sa" />
+            South America
+        </label><br/>
+        <label>
+            <input type="checkbox" name="continents[]" value="e" />
+            Europe
+        </label><br/>
+        <label>
+            <input type="checkbox" name="continents[]" value="a1" />
+            Asia
+        </label><br/>
+        <label>
+            <input type="checkbox" name="continents[]" value="a2" />
+            Australia
+        </label><br/>
+        <label>
+            <input type="checkbox" name="continents[]" value="a3" />
+            Africa
+        </label><br/>
+        <label>
+            <input type="checkbox" name="continents[]" value="a4" />
+            Antarctica
+        </label><br/><hr/>
+
+      <label class="long">
+        <div>Comments</div>
+        <textarea class="long" id="major" rows="5" name="comments" value="Computer Science"></Textarea>
+      </label>
+      <input class="button" type="submit" value="sumbmit">
+
+  </form>
+ </body>
+</html>
--- web/form/handler.php
+++ web/form/handler.php
@@ -14,26 +14,48 @@
       h4{
           display: inline;
       }
-
       
   </style>
  </head>
  <body>
 
-    <?php echo '<h4>User Name: </h4><span>'.$_POST["name"].'</span><br/>'; 
+    <?php 
+          $continents = "";
+   
+          echo '<h4>User Name: </h4><span>'.$_POST["name"].'</span><br/>'; 
           echo '<h4>User Email Address: <a href="mailto:"'.$_POST["email"].'>'.$_POST['email'].'</a></h4><br/>'; 
           echo '<h4>Major: </h4><span>'.$_POST["major"].'</span><br/>';
           echo '<h4>Comments: </h4><span>'.$_POST["comments"].'</span><br/>';
         
           if(!empty($_POST["continents"])){
 
-            #echo '<h4>Continents Visited: </h4>';
+            switch($_POST["continents"]){
+                case "na":
+                  $continents = "North America";
+                  break;
+              case "sa":
+                  $continents = "South America";
+                  break;
+              case "a1":
+                  $continents = "Asia";
+                  break;
+              case "a2":
+                  $continents = "Australia";
+                  break;
+              case "e":
+                  $continents = "Europe";
+                  break;
+              case "a3":
+                  $continents = "Africa";
+                  break;
+              case "a4":
+                  $continents = "Antarctica";
+                  break;
+              default: 
+                  $continents = $_POST["continents"];
+            }
 
-            #foreach($_POST["continents"] as $visited){
-                #echo '<h5>'.$visited.'</h5>';
-            #}
-
-            echo '<h4>Continents Visited: </h4><span>'.implode(", ",$_POST["continents"]).'</span>';
+            echo '<h4>Continents Visited: </h4><span>'.implode(", ",$continents).'</span>';
           }
               
     ?>
