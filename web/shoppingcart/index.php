f40db876a4588fd13af3b84f285fd1021f4140e2
mtunzisteven <mtunzism@byui.edu>
2021-01-27 06:01:16 +0200

    style

--- web/shoppingcart/index.php
+++ web/shoppingcart/index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta content="author" name="Mtunzi Mavuma" >
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
     <body>
