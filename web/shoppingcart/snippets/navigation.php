5d48cbfee50e7af732eca5c49a79418022098392
mtunzisteven <mtunzism@byui.edu>
2021-01-27 13:37:39 +0200

    SASS

--- web/shoppingcart/snippets/navigation.php
+++ web/shoppingcart/snippets/navigation.php
@@ -2,9 +2,5 @@
     <ul>
         <li><a href="../shoppingcart/" title="Brose Products" >Browse</a></li>
     </ul>
-    <div class="cart-container">
-            <a class="cart-link" href="../shoppingcart/cart.php" title="Shopping Cart"><img src="../shoppingcart/images/cart.png" alt="cart" /></a>
-            <div class="cart-value"></div>
-    </div>
-
+        <a class="cart-link" href="../shoppingcart/cart.php" title="Shopping Cart"><img src="../shoppingcart/images/cart.png" alt="cart" /></a>
 </nav>
