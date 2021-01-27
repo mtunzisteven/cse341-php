6fe46cd756d0c98e55b270e6134089d5b459298e
mtunzisteven <mtunzism@byui.edu>
2021-01-27 13:03:33 +0200

    SASS

--- web/shoppingcart/snippets/navigation.php
+++ web/shoppingcart/snippets/navigation.php
@@ -1,6 +1,6 @@
 <nav id="nav" class="nav">
     <ul>
-        <li>Browse</li>
+        <li><a href="../shoppingcart/" title="Brose Products" >Browse</a></li>
     </ul>
     <div class="cart-container">
             <a class="cart-link" href="../shoppingcart/cart.php" title="Shopping Cart"><img src="../shoppingcart/images/cart.png" alt="cart" /></a>
