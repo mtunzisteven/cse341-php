47586513fd272c0ba945be40e23d9ec8138dc718
mtunzisteven <mtunzism@byui.edu>
2021-01-27 05:52:35 +0200

    body nad head

--- web/shoppingcart/index.php
+++ web/shoppingcart/index.php
@@ -1,7 +1,20 @@
-<header id="header">
-    <?php require "/snippets/header.php"; ?>
-</header>
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <title>Browse</title>
+        <meta charset="utf-8">
+        <meta content="author" name="Mtunzi Mavuma" >
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <header id="header">
+            <?php require "/snippets/header.php"; ?>
+        </header>
 
-<footer id="footer">
-    <?php require "/snippets/footer.php"; ?>
-</footer>
+        <footer id="footer">
+            <?php require "/snippets/footer.php"; ?>
+        </footer>
+    </body>
+</html>
