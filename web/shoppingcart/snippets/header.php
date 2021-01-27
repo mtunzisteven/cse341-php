5bfad9cebc580f2495423854bfacf8c799d5373b
mtunzisteven <mtunzism@byui.edu>
2021-01-27 05:59:22 +0200

    path

--- web/shoppingcart/snippets/header.php
+++ web/shoppingcart/snippets/header.php
@@ -1,11 +1,11 @@
 <header id="header">
     <div class="headertop">
-        <a class="logolink" href="../shoppingcart/index.php" title="Browse Page"><img alt="logo" src="../images/logo.png" /></a>
+        <a class="logolink" href="../shoppingcart/index.php" title="Browse Page"><img alt="logo" src="../shoppingcart/images/logo.png" /></a>
     </div>
     <nav id="nav" class="nav">
-        <?php require "/navigation.php"; ?>
+        <?php require "navigation.php"; ?>
     </nav>
 
     <div class="hero"></div>
-    
+
 </header>
--- web/shoppingcart/snippets/navigation.php
+++ web/shoppingcart/snippets/navigation.php
@@ -4,7 +4,7 @@
     </ul>
     <div class="cart-container">
         <div class="cart-value"></div>
-        <a class="cart-link" href="../cart.php" title="Shopping Cart"><img src="../images/cart.png" alt="cart" /></a>
+        <a class="cart-link" href="../shoppingcart/cart.php" title="Shopping Cart"><img src="../shoppingcart/images/cart.png" alt="cart" /></a>
     </div>
 
 </nav>
