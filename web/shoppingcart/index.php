6754cca906eaee3b350b8cae17348bdccb256502
mtunzisteven <mtunzism@byui.edu>
2021-01-27 05:54:55 +0200

    body

--- web/shoppingcart/index.php
+++ web/shoppingcart/index.php
@@ -5,16 +5,16 @@
         <meta charset="utf-8">
         <meta content="author" name="Mtunzi Mavuma" >
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="/style.css">
+        <link rel="stylesheet" href="style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
     <body>
         <header id="header">
-            <?php require "/snippets/header.php"; ?>
+            <?php require "snippets/header.php"; ?>
         </header>
 
         <footer id="footer">
-            <?php require "/snippets/footer.php"; ?>
+            <?php require "snippets/footer.php"; ?>
         </footer>
     </body>
 </html>
