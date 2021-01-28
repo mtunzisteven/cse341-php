99cf864a28bde46b6b361ed24061c07031ff35f0
mtunzisteven <mtunzism@byui.edu>
2021-01-28 03:42:05 +0200

    link cart

--- web/shoppingcart/snippets/navigation.php
+++ web/shoppingcart/snippets/navigation.php
@@ -4,7 +4,7 @@
             <li><a href="../shoppingcart/" title="Brose Products" >Browse</a></li>
         </ul>
         <a class="cart-link" href="../shoppingcart/cart.php" title="Shopping Cart">
-            <span class="cart-span">cart</span><img alt="cart" src="../shoppingcart/images/cart.png" />
+        <img alt="cart" src="../shoppingcart/images/cart.png" /><div class="cart-span">cart</div>
         </a>
     </div>
 </nav>
