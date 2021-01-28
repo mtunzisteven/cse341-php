2c090ca8e9f1a1f2cdacc4ffb9196464b6c7d5bc
mtunzisteven <mtunzism@byui.edu>
2021-01-28 07:04:05 +0200

    parseInt()

--- web/shoppingcart/functions/functions.js
+++ web/shoppingcart/functions/functions.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#watch").click( function(){
 
         var invisiblevalue = $("#watchvalue").val();
-        var newV = ParseInt(invisiblevalue)+1;
+        var newV = parseInt(invisiblevalue)+1;
 
         $("#watchvalue").val(newV);
         alert("Value of invisible changed to: "+$("#watchvalue").val());
