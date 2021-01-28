a7213578f7df967b26bab4ca656ba40c3eb05cd6
mtunzisteven <mtunzism@byui.edu>
2021-01-28 06:57:26 +0200

    integer

--- web/shoppingcart/functions/functions.js
+++ web/shoppingcart/functions/functions.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#watch").click( function(){
 
         var invisiblevalue = $("#watchvalue").val();
-        var newV = invisiblevalue+1;
+        var newV = int(invisiblevalue)+1;
 
         $("#watchvalue").val(newV);
         alert("Value of invisible changed to: "+$("#watchvalue").val());
