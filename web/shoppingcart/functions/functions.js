a7a9126321a6d34faf17acf29c0b90301b22076c
mtunzisteven <mtunzism@byui.edu>
2021-01-29 15:29:44 +0200

    this

--- web/shoppingcart/functions/functions.js
+++ web/shoppingcart/functions/functions.js
@@ -1,6 +1,40 @@
 
 $(document).ready(function () {
 
+ 
+    $("#form1").on("submit" ,function(){
+        action(this);
+    });
+
+    $("#form2").on("submit" ,function(){
+        action(this);
+    });
+    
+    $("#form3").on("submit" ,function(){
+        action(this);
+    });
+
+    $("#form4").on("submit" ,function(){
+        action(this);
+    });
+    
+    $("#form5").on("submit" ,function(){
+        action(this);
+    });
+
+    $("#form6").on("submit" ,function(){
+        action(this);
+    });
+
+    $("#form7").on("submit" ,function(){
+        action(this);
+    });
+
+    $("#form8").on("submit" ,function(){
+        action(this);
+    });
+
+    /*
 
     $("#form1").on("submit" ,function(){
 
@@ -68,14 +102,14 @@ $(document).ready(function () {
 
     $("#form3").on("submit" ,function(){
 
-        var invisiblevalue = $("#maskqty").val(); //Initial qty of product selected
+        var invisiblevalue = $("#sandalsqty").val(); //Initial qty of product selected
         var newV = parseInt(invisiblevalue)+1;        //1 added per selection to qty
  
         //Setting the new qty to specific items in cart
-        $("#maskqty").val(newV);
+        $("#sandalsqty").val(newV);
         
         //Getting the name of the proxduct from input value
-        var item_name = $("#maskdata").val();
+        var item_name = $("#sandalsdata").val();
 
         //Gather form submission data
         var holder = $(this), //"holder" is just so we don't keep typing "this"
@@ -83,7 +117,7 @@ $(document).ready(function () {
             method =holder.attr("method"),
             data = {};
 
-            data[$("#maskdata").attr('name')] = item_name; //Adding item name to POST data to be sent
+            data[$("#sandalsdata").attr('name')] = item_name; //Adding item name to POST data to be sent
                    
         //The actual ajax data transfer method. Data gathered above will now be used.
         $.ajax({
@@ -163,37 +197,7 @@ $(document).ready(function () {
         return false; //With this return value, we ensure the form doesn't get submitted and reload the page
     });
 
-    $("#form6").on("submit" ,function(){
-
-        var invisiblevalue = $("#oilsqty").val(); //Initial qty of product selected
-        var newV = parseInt(invisiblevalue)+1;        //1 added per selection to qty
- 
-        //Setting the new qty to specific items in cart
-        $("#oilsqty").val(newV);
-        
-        //Getting the name of the proxduct from input value
-        var item_name = $("#oilsdata").val();
-
-        //Gather form submission data
-        var holder = $(this), //"holder" is just so we don't keep typing "this"
-            url = holder.attr("action"),
-            method =holder.attr("method"),
-            data = {};
-
-            data[$("#oilsdata").attr('name')] = item_name; //Adding item name to POST data to be sent
-        
-        //The actual ajax data transfer method. Data gathered above will now be used.
-        $.ajax({
-            type: method, 
-            url: url,
-            data: data,
-            success: function (response) {
-                console.log(response);
-            }
-        });
-
-        return false; //With this return value, we ensure the form doesn't get submitted and reload the page
-    });
+   
 
     $("#form7").on("submit" ,function(){
 
@@ -258,6 +262,71 @@ $(document).ready(function () {
 
         return false; //With this return value, we ensure the form doesn't get submitted and reload the page
     });
-
+*/
     
 });
+
+function action(form){
+    var itemQty, item_name;
+
+    switch(form) {
+        case "form1":
+            itemQty = "#watchqty";
+            item_name = "#watchdata";
+            break;
+        case "form2":
+            itemQty = "#saltsqty";
+            item_name = "#saltsdata";
+            break;
+        case "form3":
+            itemQty = "#sandalsqty";
+            item_name = "#sandalsdata";
+        case "form4":
+            itemQty = "#soillsqty";
+            item_name = "#oilsdata";
+        case "form5":
+            itemQty = "#maskqty";
+            item_name = "#maskdata";
+        case "form6":
+            itemQty = "#chanelqty";
+            item_name = "#chaneldata";
+        case "form7":
+            itemQty = "#loaferqty";
+            item_name = "#loaferdata";
+        case "form8":
+            itemQty = "#trainerqty";
+            item_name = "#trainerdata";
+        default:
+            return;
+    }
+
+
+    var invisiblevalue = $(ItemQty).val(); //Initial qty of product selected
+    var newV = parseInt(invisiblevalue)+1;        //1 added per selection to qty
+
+    //Setting the new qty to specific items in cart
+    $(ItemQty).val(newV);
+    
+    //Getting the name of the proxduct from input value
+    var item_name = $(inputName).val();
+
+    //Gather form submission data
+    var holder = $(this), //"holder" is just so we don't keep typing "this"
+        url = holder.attr("action"),
+        method =holder.attr("method"),
+        data = {};
+
+        data[$(inputName).attr('name')] = item_name; //Adding item name to POST data to be sent
+    
+    //The actual ajax data transfer method. Data gathered above will now be used.
+    $.ajax({
+        type: method, 
+        url: url,
+        data: data,
+        success: function (response) {
+            console.log(response);
+        }
+    });
+
+    return false; //With this return value, we ensure the form doesn't get submitted and reload the page
+}
