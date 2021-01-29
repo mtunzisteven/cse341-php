001fcb122c3377106d4cb73b32559997dbb367f4
mtunzisteven <mtunzism@byui.edu>
2021-01-29 13:00:16 +0200

    n

--- web/shoppingcart/index.php
+++ web/shoppingcart/index.php
@@ -16,7 +16,7 @@
         <div class="form">
             <form id="form" method="post" action="cart.php" class="product-container">
                 <label id="watch-label">
-                    <img class="products" alt="watch" src="images/ watch.jpg" />
+                    <img class="products" alt="watch" src="images/watch.jpg" />
                     Fossil Watch<br/>$150
                     
                 </label>
@@ -26,7 +26,7 @@
                 <input class="buttons" id="watch" type="submit" name="watch" value="Add to Cart" />
             </form>
 
-            <form id="form" method="post" action="" class="product-container">
+            <form id="form" method="post" action="cart.php" class="product-container">
                 <label  id="salts-label">
                     <img class="products" alt="salts" src="images/salts.jpg" />
                     Bath Salts<br/>$15
@@ -34,10 +34,10 @@
                 <input class="invisibles proname" type="text" id="saltsdata" name="saltsdata" value="Bath Salts" />
                 <input class="invisibles qty" type="text" id="qty" name="saltsqty" value="<?php if($_SESSION["saltsqty"]<1){echo 0 ;} else{echo $_SESSION["saltsqty"];} ?>" />
                 <input class="invisibles src" type="text" id="src" name="saltssrc" value="images/salts.jpg" /> 
-                <input class="buttons" id="salts" type="button" name="salts" value="Add to Cart" />
+                <input class="buttons" id="salts" type="submit" name="salts" value="Add to Cart" />
             </form>
 
-            <form method="post" action="" class="product-container">
+            <form method="post" action="cart.php" class="product-container">
                 <label>
                     <img class="products" alt="watch" src="images/sandals.jpg" />
                     Brikestock Sandals<br/>$50    
@@ -45,10 +45,10 @@
                 <input class="invisibles proname" type="text" id="sandalsdata" name="sandalsdata" value="Brikestock Sandals" />
                 <input class="invisibles qty" type="text" id="qty" name="sandalsqty" value="<?php if($_SESSION["watchqty"]<1){echo 0 ;} else{echo $_SESSION["watchqty"];} ?>" />
                 <input class="invisibles src" type="text" id="src" name="sandalssrc" value="images/sandals.jpg" />  
-                <input class="buttons" type="button" id="sandals" name="sandals" value="Add to Cart" />
+                <input class="buttons" type="button" id="submit" name="sandals" value="Add to Cart" />
             </form>
 
-            <form method="post" action="" class="product-container">
+            <form method="post" action="cart.php" class="product-container">
                 <label class="products-label">
                     <img class="products" alt="watch" src="images/oil.jpg" />
                     Coconut Oil<br/>$10
@@ -56,10 +56,10 @@
                 <input class="invisibles proname" type="text" id="oilsdata" name="oilsdata" value="Coconut Oil" /> 
                 <input class="invisibles qty" type="text" id="qty" name="oilsqty" value="<?php if($_SESSION["oilsqty"]<1){echo 0 ;} else{echo $_SESSION["oilsqty"];} ?>" />
                 <input class="invisibles src" type="text" id="src" name="oilssrc" value="images/oil.jpg" /> 
-                <input class="buttons" type="button" id="oil" name="oil" value="Add to Cart" />
+                <input class="buttons" type="submit" id="oil" name="oil" value="Add to Cart" />
             </form>
 
-            <form method="post" action="" class="product-container">
+            <form method="post" action="cart.php" class="product-container">
                 <label>
                     <img class="products" alt="watch" src="images/mask.jpg" />
                     Face Mask<br/>$50
@@ -67,10 +67,10 @@
                 <input class="invisibles proname" type="text" id="maskdata" name="maskdata" value="Face Mask" /> 
                 <input class="invisibles qty" type="text" id="qty" name="maskqty" value="<?php if($_SESSION["maskqty"]<1){echo 0 ;} else{echo $_SESSION["maskqty"];} ?>" />
                 <input class="invisibles src" type="text" id="src" name="masksrc" value="images/mask.jpg" /> 
-                <input class="buttons" type="button" id="mask" name="mask" value="Add to Cart" />
+                <input class="buttons" type="submit" id="mask" name="mask" value="Add to Cart" />
             </form>
 
-            <form method="post" action="" class="product-container">
+            <form method="post" action="cart.php" class="product-container">
                 <label>
                     <img class="products" alt="watch" src="images/chanel.jpg" />
                     Gabriel Chanel<br/>$150
@@ -78,10 +78,10 @@
                 <input class="invisibles proname" type="text" id="chaneldata" name="chaneldata" value="Gabriel Chanel" />
                 <input class="invisibles qty" type="text" id="qty" name="chanelqty" value="<?php if($_SESSION["chanelqty"]<1){echo 0 ;} else{echo $_SESSION["chanelqty"];} ?>" />
                 <input class="invisibles src" type="text" id="src" name="chanelsrc" value="images/chanel.jpg" />
-                <input class="buttons" type="button" id="gabriel" name="gabriel" value="Add to Cart" />
+                <input class="buttons" type="submit" id="gabriel" name="gabriel" value="Add to Cart" />
             </form>
 
-            <form method="post" action="" class="product-container">
+            <form method="post" action="cart.php" class="product-container">
                 <label>
                     <img class="products" alt="watch" src="images/loafer.jpg" />
                     Green Loafer<br/>$100
@@ -89,10 +89,10 @@
                 <input class="invisibles proname" type="text" id="loaferdata" name="loaferdata" value="Green Loafer" />
                 <input class="invisibles qty" type="text" id="qty" name="loaferqty" value="<?php if($_SESSION["loaferqty"]<1){echo 0 ;} else{echo $_SESSION["loaferqty"];} ?>" />
                 <input class="invisibles src" type="text" id="src" name="loafersrc" value="images/loafer.jpg" />
-                <input class="buttons" type="button" id="loafer" name="loafer" value="Add to Cart" />
+                <input class="buttons" type="submit" id="loafer" name="loafer" value="Add to Cart" />
             </form>
 
-            <form method="post" action="" class="product-container">
+            <form method="post" action="cart.php" class="product-container">
                 <label>
                     <img class="products" alt="watch" src="images/trainer.jpg" />
                     Nike Trainer<br/>$190
@@ -100,7 +100,7 @@
                 <input class="invisibles proname" type="text" id="trainerdata" name="trainerdata" value="Nike Trainer" />
                 <input class="invisibles qty" type="text" id="qty" name="trainerqty" value="<?php if($_SESSION["trainerqty"]<1){echo 0 ;} else{echo $_SESSION["trainerqty"];} ?>" />
                 <input class="invisibles src" type="text" id="src" name="trainersrc" value="images/trainer.jpg" />
-                <input class="buttons" type="button" id="trainer" name="trainer" value="Add to Cart" />
+                <input class="buttons" type="submit" id="trainer" name="trainer" value="Add to Cart" />
             </form>
         </div>    
         <?php require "snippets/footer.php"; ?>
