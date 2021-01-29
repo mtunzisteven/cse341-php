b85495b4e617f83718d31cdae344c5e043198de3
mtunzisteven <mtunzism@byui.edu>
2021-01-29 18:22:21 +0200

    n

--- web/shoppingcart/index.php
+++ web/shoppingcart/index.php
@@ -87,7 +87,7 @@
                     Green Loafer<br/>$100
                 </label>
                 <input class="invisibles proname" type="text" id="loaferdata" name="loaferdata" value="Green Loafer" />
-                <input class="invisibles qty" type="text" id="qty" name="loaferqty" value=<?php if($_SESSION["loaferqty"]<1){0 ;} else{ $_SESSION["loaferqty"];} ?> />
+                <input class="invisibles qty" type="text" id="qty" name="loaferqty" value="0" />
                 <input class="invisibles src" type="text" id="src" name="loafersrc" value="images/loafer.jpg" />
                 <input class="buttons" type="submit" id="loafer" name="loafer" value="Add to Cart" />
             </form>
