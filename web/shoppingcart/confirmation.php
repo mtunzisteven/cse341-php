3d174cc05c595b86415025e635ee7265007e4845
mtunzisteven <mtunzism@byui.edu>
2021-01-31 00:26:08 +0200

    checkout

--- web/shoppingcart/confirmation.php
+++ web/shoppingcart/confirmation.php
@@ -1,7 +1,43 @@
-<header id="header">
-    <?php require "/snippets/header.php"; ?>
-</header>
+<?php session_start(); 
 
-<footer id="footer">
-    <?php require "/snippets/footer.php"; ?>
-</footer>
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <title>Checkout</title>
+        <meta charset="utf-8">
+        <meta content="author" name="Mtunzi Mavuma" >
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </head>
+    <body>
+        
+        <?php require "snippets/header.php"; ?>
+        
+        <div class="form" >
+            <h3 class="checkout-form-title">Thank you for Placing Your Order</h3>
+            <div class="order-summary">
+
+                <div class="address">
+                <?php 
+                    
+                ?>
+                </div>
+
+                <div class="products">
+                <?php 
+                    
+                ?>
+                </div>
+
+            </div>
+
+        </div>
+        
+        <?php require "snippets/footer.php"; ?>
+        
+    </body>
+
+</html>
