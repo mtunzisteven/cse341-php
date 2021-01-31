43b1fc7dc0dac05828c2a873a9fe8b85e708c137
mtunzisteven <mtunzism@byui.edu>
2021-01-31 02:13:02 +0200

    shopping cart

--- web/home/assignments.php
+++ web/home/assignments.php
@@ -24,7 +24,7 @@
    ?>
 
   <div class="container">
-      <h2 class="soon">Assignment: Shopping Car</h2>
+      <p class="soon"><a href="../shoppingcart/" title="Shopping Cart">Assignment: Shopping Car</a></p>
   </div>
 
   <?php
