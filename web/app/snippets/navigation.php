1ed94e1417d4ead3b7062d8067e7ea3aab47cd30
mtunzisteven <mtunzism@byui.edu>
2021-02-08 11:04:19 +0200

    hero and nav items

--- web/app/snippets/navigation.php
+++ web/app/snippets/navigation.php
@@ -1,9 +1,9 @@
 <nav id="page-nav">
     <ul>
-        <li><a class="links" href="/" title="Home Page">Home</a></li>
-        <li><a class="links" href="/" title="Classic Cars Page">About</a></li>
-        <li><a class="links" href="/" title="Sports Cars Page">Developers</a></li>
-        <li><a class="links" href="/" title="SUV Page">Projects</a></li>
-        <li><a class="links" href="/" title="Trucks Page">Request Developer</a></li>
+        <li><a class="links" href="/" title="Home">Home</a></li>
+        <li><a class="links" href="/" title="About">About</a></li>
+        <li><a class="links" href="/" title="Developers">Developers</a></li>
+        <li><a class="links" href="/" title="Projects">Projects</a></li>
+        <li><a class="links" href="/" title="Request">Request Developer</a></li>
     </ul>
 </nav>
