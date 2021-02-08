9d48ed98dd2bb5aa2076c5a7c0d9d58d50cd11c5
mtunzisteven <mtunzism@byui.edu>
2021-02-08 12:34:10 +0200

    header enclose removal

--- web/app/snippets/header.php
+++ web/app/snippets/header.php
@@ -1,6 +1,4 @@
 <header id="page_header">
-    <div class="header-top">
         <img class="logo" src="/" alt="logo image"/>
         <a class="account" href="/cs313-php/controller/?action=login">My Account</a>
-    </div>
 </header>
