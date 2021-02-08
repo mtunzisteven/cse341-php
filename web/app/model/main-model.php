32d7d119e8fe1b36086c744224d05f233e7816ab
mtunzisteven <mtunzism@byui.edu>
2021-02-08 17:00:30 +0200

    developer

--- web/app/model/main-model.php
+++ web/app/model/main-model.php
@@ -38,9 +38,13 @@ $stmt->bindValue(':rating', $rating, PDO::PARAM_STR);
 $stmt->execute();
 $developer = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo var_dump($developer);
+
 $stmt = $db->prepare('SELECT * FROM customer');
 $stmt->bindValue(':firstName', $firstName, PDO::PARAM_STR);
 $stmt->bindValue(':lastName', $lastName, PDO::PARAM_STR);
 $stmt->bindValue(':profileImage', $profileImage, PDO::PARAM_STR);  
 $stmt->execute();
 $customer = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo var_dump($customer);
