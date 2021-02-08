d69b9ba257a57d068c87e6c0fe0b0dccd2a6efd0
mtunzisteven <mtunzism@byui.edu>
2021-02-08 16:46:33 +0200

    model

--- web/app/index.php
+++ web/app/index.php
@@ -4,32 +4,12 @@
 
 echo "Started!<br/>";
 
+require_once "model/main-model.php";
 
-try{
-
-    $dbUrl = getenv('DATABASE_URL');
-
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo 'Success!';
-
-}catch (PDOException $ex){
-
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL){
+    $action = filter_input(INPUT_GET, 'action');
 }
-
 
 //For Testing purposes
 echo var_dump($developer);
