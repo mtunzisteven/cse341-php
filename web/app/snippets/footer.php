ddabefa0d2363bc3c7ea2d736f6cf0a14667fa83
mtunzisteven <mtunzism@byui.edu>
2021-02-08 10:34:39 +0200

    styling items

--- web/app/snippets/footer.php
+++ web/app/snippets/footer.php
@@ -1,5 +1,4 @@
 <footer id="page-footer">
     <p class="footer-text">&copy Developer Hire. All rights reserved.<br/>
-    All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.<br/>
     Last Updated 6 February, 2021</p>
 </footer>
