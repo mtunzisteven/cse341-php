461b90d904a16012b7a9b41857cc160d18ed9b43
mtunzisteven <mtunzism@byui.edu>
2021-02-09 15:23:12 +0200

    login page

--- web/app/accounts/index.php
+++ web/app/accounts/index.php
@@ -1,15 +1,20 @@
 <?php
 
-echo "accounts here!";
+echo "accounts here!<br/>";
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL){
     $action = filter_input(INPUT_GET, 'action');
 }
 
+echo "accounts filtered!";
+
 switch($action){
     case 'login':
+
+        echo "login selected!<br/>";
         include "/app/views/login.php";
+        echo "path here!";
         break;
     case 'reg':
         include "/app/views/register.php";
