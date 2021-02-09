69c2d794774b40c8114fe40e8ee459222086aad5
mtunzisteven <mtunzism@byui.edu>
2021-02-09 05:05:12 +0200

    include paths

--- web/app/views/home.php
+++ web/app/views/home.php
@@ -8,8 +8,8 @@
     </head>
     <body>
         <main class="content">
-            <?php include '/web/app/snippets/header.php'; ?>
-            <?php include '/web/app/snippets/navigation.php'; ?>
+            <?php include 'snippets/header.php'; ?>
+            <?php include 'snippets/navigation.php'; ?>
 
             <div class="hero">     
                 <h1 id="title">Home</h1>
@@ -37,7 +37,7 @@
                 </section>
             </div>  
            
-            <?php include 'app/snippets/footer.php'; ?>
+            <?php include 'snippets/footer.php'; ?>
         </main>
     </body>
 </html>
