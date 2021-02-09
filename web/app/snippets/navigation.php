1e23a4d6046dd703726e2d37863a702570f503cf
mtunzisteven <mtunzism@byui.edu>
2021-02-09 14:01:18 +0200

    style

--- web/app/snippets/navigation.php
+++ web/app/snippets/navigation.php
@@ -3,7 +3,5 @@
         <li><a class="links" href="../app/?action=home" title="Home">Home</a></li>
         <li><a class="links" href="../app/?action=about" title="About">About</a></li>
         <li><a class="links" href="../app/?action=developers" title="Developers">Developers</a></li>
-        <li><a class="links" href="../app/?action=projects" title="Projects">Projects</a></li>
-        <li><a class="links" href="../app/?action=request" title="Request">Request Developer</a></li>
     </ul>
 </nav>
