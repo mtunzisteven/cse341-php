bbbfc9377b3faecbe41af9e3f80ad3b28bec085c
mtunzisteven <mtunzism@byui.edu>
2021-02-09 08:44:10 +0200

    left column

--- web/app/views/home.php
+++ web/app/views/home.php
@@ -18,6 +18,7 @@
             <p class="intro">This application will help you get a developer to complete your project. Just select a developer and request their services.</p>
             <div class="sections">
                 <section class="left-section"> 
+                    <h4>Projects</h4>
                     <?php
                         include "/app/main-model.php";
 
@@ -28,6 +29,7 @@
                 </section>
 
                 <section class="right-section"> 
+                    <h4>Developers</h4>
                     <?php
 
                         foreach($developer as $row){
