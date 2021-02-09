e886685fc9c2f21e7c4f0317fbf505ebb15daec3
mtunzisteven <mtunzism@byui.edu>
2021-02-09 15:32:07 +0200

    nav

--- web/app/snippets/navigation.php
+++ web/app/snippets/navigation.php
@@ -1,7 +1,7 @@
 <nav id="page-nav">
     <ul>
-        <li><a class="links" href="../app/?action=home" title="Home">Home</a></li>
-        <li><a class="links" href="../app/?action=about" title="About">About</a></li>
-        <li><a class="links" href="../app/?action=developers" title="Developers">Developers</a></li>
+        <li><a class="links" href="../?action=home" title="Home">Home</a></li>
+        <li><a class="links" href="../?action=about" title="About">About</a></li>
+        <li><a class="links" href="../?action=developers" title="Developers">Developers</a></li>
     </ul>
 </nav>
