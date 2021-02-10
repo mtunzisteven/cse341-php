fdb3b07c3d51d41a7b11a5768947b0b1998c2d21
mtunzisteven <mtunzism@byui.edu>
2021-02-10 12:40:18 +0200

    check POST controller value error

--- web/app/models/account-model.php
+++ web/app/models/account-model.php
@@ -8,18 +8,23 @@ echo "Account model inserts connection<br/>";
 
 $db = db_connect();
 
+echo "Setting User Data<br/>";
 //Set developer data
 $stmt = $db->prepare('INSERT INTO developer (firstname, lastname, username, phone, email, passwrd) VALUES (?,?,?,?,?,?)');
+echo "??<br/>";
 
-$stmt->bindParam("sssiss", $firstname, $lastname, $username,$phone,$email,$password);
+$stmt->bindParam("sssiss", $firstnam, $lastnam, $usernam,$phon,$emai,$passwor);
+echo "Bound<br/>";
 
-$firstname="Lusanda";
-$lastname="Mavuma";
-$username="lusysuh";
+$firstnam="Lusanda";
+$lastnam="Mavuma";
+$usernam="lusysuh";
 $phone=0732124365;
-$email="luhh1973@gmail.com";
-$password="passwrd";
+$emai="luhh1973@gmail.com";
+$passwor="passwrd";
 
+echo "Values<br/>";
 $stmt->execute();
+
 echo $stmt;
 $stmt->closeCursor();
