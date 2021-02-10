0a7339152e707fb7e6ac6b9d2a753f459165ee22
mtunzisteven <mtunzism@byui.edu>
2021-02-10 12:11:47 +0200

    quotes

--- web/app/models/account-model.php
+++ web/app/models/account-model.php
@@ -9,7 +9,7 @@ require_once "/app/connector/connection.php";
 $db = db_connect();
 
 //Set developer data
-$stmt = $db->prepare('INSERT INTO developer (firstname, lastname, username, phone, email, passwrd) VALUES ("Lusanda", "Mavuma","lusysuh" , 0732124365, luhh1973@gmail.com, "passwrd")');
+$stmt = $db->prepare("INSERT INTO 'developer' ('firstname', 'lastname', 'username', 'phone', 'email', 'passwrd') VALUES ('Lusanda', 'Mavuma','lusysuh' , 0732124365, luhh1973@gmail.com, 'passwrd')");
 $stmt->execute();
 echo "Account model Inserts passed";
 $stmt->closeCursor();
