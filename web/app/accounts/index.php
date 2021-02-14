f9048f1c1f7fa548ea47f48cfdd89a584112f1fd
mtunzisteven <mtunzism@byui.edu>
2021-02-14 05:11:19 +0200

    testing

--- web/app/accounts/index.php
+++ web/app/accounts/index.php
@@ -37,7 +37,7 @@ switch($action){
         if($logins===1){
             // fetch all developer data
             $develepoer_data = getDeveloper_data($username, $passwrd);
-            //echo "Hey!"; exit;
+            echo "Hey!"; exit;
             // set session data
             $_SESSION['dev_firstname'] = $develepoer_data['firstname'];
             $_SESSION['dev_lastname'] = $develepoer_data['lastname'];
