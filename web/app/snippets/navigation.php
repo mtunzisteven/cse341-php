64669b5b921d98acf45f96e578ad6952c1e8c1ca
mtunzisteven <mtunzism@byui.edu>
2021-02-14 07:15:46 +0200

    account to accounts

--- web/app/snippets/navigation.php
+++ web/app/snippets/navigation.php
@@ -1,7 +1,7 @@
 <nav id="page-nav">
     <ul>
-        <li><a class="links" href="<?php if(isset($_SESSION['dev_firstname'])){echo '/app/account/?action=home';}else{echo '/app/?action=home';} ?>" title="Home">Home</a></li>
-        <li><a class="links" href="<?php if(isset($_SESSION['dev_firstname'])){echo '/app/account/?action=about';}else{echo '/app/?action=about';} ?>" title="About">About</a></li>
-        <li><a class="links" href="<?php if(isset($_SESSION['dev_firstname'])){echo '/app/account/?action=developers';}else{echo '/app/?action=developers';} ?>" title="Developers">Developers</a></li>
+        <li><a class="links" href="<?php if(isset($_SESSION['dev_firstname'])){echo '/app/accounts/?action=home';}else{echo '/app/?action=home';} ?>" title="Home">Home</a></li>
+        <li><a class="links" href="<?php if(isset($_SESSION['dev_firstname'])){echo '/app/accounts/?action=about';}else{echo '/app/?action=about';} ?>" title="About">About</a></li>
+        <li><a class="links" href="<?php if(isset($_SESSION['dev_firstname'])){echo '/app/accounts/?action=developers';}else{echo '/app/?action=developers';} ?>" title="Developers">Developers</a></li>
     </ul>
 </nav>
