1681e782099f32274025f1a8cc35b42bc3a92717
mtunzisteven <mtunzism@byui.edu>
2021-02-14 08:11:17 +0200

    path from root fix

--- web/app/views/template.php
+++ web/app/views/template.php
@@ -1,15 +1,15 @@
 <!doctype html>
 <html lang="en-us">
     <head>
-        <title>home</title>
+        <title>Template</title>
         <meta charset="utf-8">
         <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
         <link href="/app/css/style.css" media="screen" rel="stylesheet">
     </head>
     <body>
         <main class="content">
-            <?php include 'snippets/header.php'; ?>
-            <?php include 'snippets/navigation.php'; ?>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/app/snippets/header.php'; ?>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/app/snippets/navigation.php'; ?>
 
             <div class="hero">     
                 <h1 id="title">Home</h1>
@@ -17,7 +17,7 @@
 
 
            
-            <?php include 'snippets/footer.php'; ?>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/app/snippets/footer.php'; ?>
         </main>
     </body>
 </html>
