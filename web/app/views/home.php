df4adc2ebb471931c2f8d86af93d1bd77d6a3904
mtunzisteven <mtunzism@byui.edu>
2021-02-20 18:28:42 +0200

    unclosed div fix

--- web/app/views/home.php
+++ web/app/views/home.php
@@ -34,7 +34,7 @@
                                     echo  '<div class="dev-inner-container"> <p class="dev-contacts">'.'Phone: '.$row['phone'].' <br/>Email: '.$row['email'].'</p>';
                                         echo  '<a class="dev-link" href="../app/account/index.php?action=profile&user='.$row['developerid'].'" title="Request a Developer" >Request</a>';
                                         echo  '<div class="dev-text-container"><h4 class="center dev-countries">'.$row['country'].'</h4>';
-                                        echo  '<i class="center dev-cities">'.$row['city'].'</i></div></div>';
+                                        echo  '<i class="center dev-cities">'.$row['city'].'</i></div></div></div>';
                             echo  '</div><br/>';
                         }
                     ?>
