e7d3a765a38528973453d60c1512526d8af2a92a
mtunzisteven <mtunzism@byui.edu>
2021-02-20 15:31:11 +0200

    developer page images sizing

--- web/app/views/developers.php
+++ web/app/views/developers.php
@@ -20,7 +20,7 @@
             <?php
 
                 foreach($developer as $row){
-                    echo '<a class="dev-img-link"><img class="dev-img" src="images/'.$row['profileimage'].'" alt="profile image" /><h4 class="dev-img-name">'.$row['firstname'].'</h4></a>';
+                    echo '<a class="dev-img-link"><img class="dev-page-img" src="images/'.$row['profileimage'].'" alt="profile image" /><h4 class="dev-img-name">'.$row['firstname'].'</h4></a>';
                 }
 
             ?>
