bb76ef64219a9e3a50f11b1f73df6b8b4b496a6b
mtunzisteven <mtunzism@byui.edu>
2021-02-20 21:06:35 +0200

    style

--- web/app/snippets/header.php
+++ web/app/snippets/header.php
@@ -1,4 +1,4 @@
 <header id="page_header">
-        <img class="logo" src="/" alt="logo image"/>
+        <img class="logo" src="/app/images/logo.png" alt="logo image"/>
         <a class="account" href="/app/accounts/index.php?action=login"><?php if(isset($_SESSION['dev_firstname'])){echo $_SESSION['dev_firstname'];} ?> &#128100; My Account</a>
 </header>
